a7d484236b35afaf0e0f2dd8cb769874cd8c995d
Daria Trubnikova <daria.trubnikova@gmail.com>
2021-02-25 17:07:08 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(Array.isArray(array) && ( array.length > 0 )) {
+    return Math.min.apply(null, array);
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array) && ( array.length > 0 )) {
+    return Math.max.apply(null, array);
+  } else {
+    return 0;
+  }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array) && ( array.length > 0 )) {
+    let sum = 0;
+    for(let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return (sum / array.length);
+  } else {
+    return 0;
+  }
 }
